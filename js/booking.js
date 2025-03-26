b428b04387726b4b5549b93e1b6c75d96ac92f74
ThatoMathe <thatomathe007@gmail.com>
2025-03-26 19:35:29 +0200

    created a js folder

--- js/booking.js
+++ js/booking.js
@@ -1,10 +1,10 @@
+
 function bookAppointment() {
     const clientName = document.getElementById("name").value;
     const date = document.getElementById("date").value;
     const time = document.getElementById("time").value;
     const service = document.getElementById("service").value;
   
-    // Save to Firestore
     db.collection("bookings").add({
       clientName: clientName,
       date: date,
@@ -12,10 +12,6 @@ function bookAppointment() {
       service: service,
       status: "Pending"
     })
-    .then(() => {
-      alert("Booking saved! 🎉");
-    })
-    .catch((error) => {
-      alert("Error: " + error);
-    });
+    .then(() => alert("Booking saved! 🎉"))
+    .catch((error) => alert("Error: " + error));
   }
